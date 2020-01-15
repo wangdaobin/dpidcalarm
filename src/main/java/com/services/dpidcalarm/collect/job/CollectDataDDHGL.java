5675d3eed7ac32a7214c9e5710b7b787b9554650
zhangtao331703405 <331703405@qq.com>
2020-01-15 18:34:54 +0800

    1、增加单独合格率获取接口； 2、调整积分电量公司中文名称编码方式：

---
+++ src/main/java/com/services/dpidcalarm/collect/job/CollectDataDDHGL.java
@@ -0,0 +1,180 @@
+package com.services.dpidcalarm.collect.job;
+
+import com.services.dpidcalarm.collect.bean.ErrorResult;
+import com.services.dpidcalarm.collect.bean.SvgUtil;
+import com.services.dpidcalarm.datainfo.bean.DataInfo;
+import com.services.dpidcalarm.sconf.bean.PropertiesConfiguration;
+import com.services.dpidcalarm.smslog.bean.SmSendLog;
+import com.services.dpidcalarm.utils.CommonUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Timestamp;
+import java.util.*;
+
+/**
+ * @Description：采集单独合格率
+ * @Author：zhangtao
+ * @Date：2020/1/15 0015 9:21:11
+ */
+public class CollectDataDDHGL {
+
+    //电站列表
+    public static final List<String> stations = Arrays.asList(new String[] { "南湖站", "惠民站", "木洞站", "接龙站", "纳溪沟站", "安澜站", "鹿角站", "永隆站", "界石站", "五布站", "广阳站", "双河站", "姜家站", "苏家湾站", "东港站", "莲池站", "柳银站", "桥口坝站", "黄桷垭站", "回龙湾站", "百步梯站", "响水洞站", "阳光一百站", "弹子石站", "长生站", "海棠站", "花溪站", "峡口站", "迎龙站", "武堂站", "高峰寺站", "南彭站", "鱼洞站", "南坪站", "李家沱站", "土桥站", "天文站", "梓桐站", "丹桂站", "金竹站", "二塘站", "光国站", "天星寺站", "龙门浩站", "龙洲湾站", "柏子桥站", "书房站", "花红站", "白马山站", "金家岩站", "虎啸站", "四公里站", "鸡冠石站", "走马羊站" });
+
+    //客户端句柄
+    private CloseableHttpClient client = null;
+    private Logger logger = LoggerFactory.getLogger(CollectDataSPOPS.class);
+
+
+
+    /**
+     * 登录服务
+     * @return
+     * @throws IOException
+     */
+    public boolean login(String loginURL, String user, String pwd) throws IOException {
+
+        this.client = HttpClients.createDefault();
+        try {
+            //创建登录post对象，登录URL
+            HttpPost post = new HttpPost("http://10.55.1.99:8000/svg/SvgLoginServlet");
+            //post参数 账号&密码
+            HttpEntity requestEntity = new StringEntity("wzdd" + "&" + "000");
+            post.setEntity(requestEntity);
+            //执行登录请求
+            HttpResponse response = client.execute(post);
+            this.logger.info("单独合格率登录结果：" + EntityUtils.toString(response.getEntity()));
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 获取获取单独合格率的结果
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    public String getSvgImageForKHZB(String url) throws IOException {
+        String result = null;
+
+
+        url = "http://10.55.1.99:8000/svg/file/SvgTableGraphServlet" + "?timeStamp=" +  (new Date()).getTime();
+        //请求指标的地址：根目录 + 相对目录 + 时间戳
+        HttpPost post = new HttpPost(url);
+        //请求参数，可能需要根据实际情况在调试：
+        String param = "<root><TableData table_id='2038' app_id='201100' HisDataType='0' col_name_list='fa_name,name,sum_st,rate_dv,srate_meas,sum_meas,good_meas,bad_point,sum_v,good_v,sum_ln,good_ln,sum_xf,good_xf,visible,sum_ld,good_ld,sum_un,good_un' sql=\"select fa_name,name,sum_st,rate_dv,srate_meas,sum_meas,good_meas,bad_point,sum_v,good_v,sum_ln,good_ln,sum_xf,good_xf,visible,sum_ld,good_ld,sum_un,good_un from pasdv where visible = 1 \" finish_num='0' record_num='0' value='' m_SQL=\" f15 = 1 \" m_WhereFieldList_string=\"f15\" m_FieldInfoVec_string=\"0,f0,1,-1,-1,-1,-1;1,f1,7,-1,-1,-1,-1;2,f2,6,-1,-1,-1,-1;3,f3,12,-1,-1,-1,-1;4,f4,15,-1,-1,-1,-1;5,f5,143,-1,-1,-1,-1;6,f6,20,-1,-1,-1,-1;7,f7,21,-1,-1,-1,-1;8,f8,23,-1,-1,-1,-1;9,f9,28,-1,-1,-1,-1;10,f10,29,-1,-1,-1,-1;11,f11,24,-1,-1,-1,-1;12,f12,25,-1,-1,-1,-1;13,f13,30,-1,-1,-1,-1;14,f14,31,-1,-1,-1,-1;15,f15,11,-1,-1,-1,-1;16,f16,62,-1,-1,-1,-1;17,f17,63,-1,-1,-1,-1;18,f18,26,-1,-1,-1,-1;19,f19,27,-1,-1,-1,-1\" m_StatusQueryVec_string=\"\"/></root>";
+        HttpEntity requestEntity = new StringEntity(param);
+        post.setEntity(requestEntity);
+        //执行请求
+        HttpResponse response = this.client.execute(post);
+        this.logger.info("单独合格率获取--原始结果：" + EntityUtils.toString(response.getEntity()));
+        //请求的返回值
+        InputStream is = response.getEntity().getContent();
+        //处理解析结果
+        result = SvgUtil.svgzHandler(is, "svg");
+
+        this.logger.info("单独合格率--整理结果：" + result);
+
+        return result;
+    }
+
+    /**
+     *
+     * @param
+     * @return
+     * @throws IOException
+     */
+    public String getNananTestError() {
+        try {
+            String result = null;
+            HttpPost post = new HttpPost("http://10.55.1.99:8000/svg/file/SvgTableGraphServlet?timeStamp=" + (new Date()).getTime());
+            String param = "%3Croot%3E%3CTableData+table_id%3D%272064%27+app_id%3D%27201100%27+HisDataType%3D%271081085952%27+col_name_list%3D%27dv_name%2Cst_id%2Cdescr%2Cmeasure_type%2Cvalue_%2Cvalue_pre%2Cvalue_res%2Cvalue_r%2Cbase%27+sql%3D%22select+dv_name%2Cst_id%2Cdescr%2Cmeasure_type%2Cvalue_%2Cvalue_pre%2Cvalue_res%2Cvalue_r%2Cbase+from+pas_allmeasure+where+descr+LIKE+%27%25%E5%8D%97%E5%B2%B8%25%27+%22+finish_num%3D%270%27+record_num%3D%270%27+value%3D%27%27+m_SQL%3D%22+f3+LIKE+%27%25%E5%8D%97%E5%B2%B8%25%27+%22+m_WhereFieldList_string%3D%22f3%22+m_FieldInfoVec_string%3D%220%2Cf0%2C1%2C-1%2C-1%2C-1%2C-1%3B1%2Cf1%2C2%2C-1%2C-1%2C-1%2C-1%3B2%2Cf2%2C4%2C-1%2C-1%2C-1%2C-1%3B3%2Cf3%2C5%2C-1%2C-1%2C-1%2C-1%3B4%2Cf4%2C6%2C-1%2C-1%2C-1%2C-1%3B5%2Cf5%2C7%2C-1%2C-1%2C-1%2C-1%3B6%2Cf6%2C8%2C-1%2C-1%2C-1%2C-1%3B7%2Cf7%2C9%2C-1%2C-1%2C-1%2C-1%3B8%2Cf8%2C12%2C-1%2C-1%2C-1%2C-1%3B9%2Cf9%2C10%2C-1%2C-1%2C-1%2C-1%22+m_StatusQueryVec_string%3D%22%22%2F%3E%3C%2Froot%3E";
+            HttpEntity requestEntity = new StringEntity("%3Croot%3E%3CTableData+table_id%3D%272064%27+app_id%3D%27201100%27+HisDataType%3D%271081085952%27+col_name_list%3D%27dv_name%2Cst_id%2Cdescr%2Cmeasure_type%2Cvalue_%2Cvalue_pre%2Cvalue_res%2Cvalue_r%2Cbase%27+sql%3D%22select+dv_name%2Cst_id%2Cdescr%2Cmeasure_type%2Cvalue_%2Cvalue_pre%2Cvalue_res%2Cvalue_r%2Cbase+from+pas_allmeasure+where+descr+LIKE+%27%25%E5%8D%97%E5%B2%B8%25%27+%22+finish_num%3D%270%27+record_num%3D%270%27+value%3D%27%27+m_SQL%3D%22+f3+LIKE+%27%25%E5%8D%97%E5%B2%B8%25%27+%22+m_WhereFieldList_string%3D%22f3%22+m_FieldInfoVec_string%3D%220%2Cf0%2C1%2C-1%2C-1%2C-1%2C-1%3B1%2Cf1%2C2%2C-1%2C-1%2C-1%2C-1%3B2%2Cf2%2C4%2C-1%2C-1%2C-1%2C-1%3B3%2Cf3%2C5%2C-1%2C-1%2C-1%2C-1%3B4%2Cf4%2C6%2C-1%2C-1%2C-1%2C-1%3B5%2Cf5%2C7%2C-1%2C-1%2C-1%2C-1%3B6%2Cf6%2C8%2C-1%2C-1%2C-1%2C-1%3B7%2Cf7%2C9%2C-1%2C-1%2C-1%2C-1%3B8%2Cf8%2C12%2C-1%2C-1%2C-1%2C-1%3B9%2Cf9%2C10%2C-1%2C-1%2C-1%2C-1%22+m_StatusQueryVec_string%3D%22%22%2F%3E%3C%2Froot%3E");
+            post.setEntity(requestEntity);
+            HttpResponse response = this.client.execute(post);
+            InputStream is = response.getEntity().getContent();
+            result = SvgUtil.svgzHandler(is, "svg");
+            this.logger.info("getNananTestError单独合格率--结果：" + result);
+            return result;
+        }catch (Exception e) {
+            this.logger.info("getNananTestError单独合格率--出错：" + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+
+
+    /**
+     *
+     * @param svgResult
+     */
+    public float getResultDDHGL(String  svgResult) {
+        if(null==svgResult || "".equals(svgResult)) {
+            this.logger.info("获取的svgResult为空或者null，不处理：");
+            return -1;
+        }
+        //获取厂站合格率
+        Map<String,Float> qulifyRateMap = SvgUtil.getQulifyRate(svgResult);
+        if(qulifyRateMap.size()==0) {
+            logger.error("qulifyRateMap为空,单独合格率获取失败");
+            return -1;
+        }
+
+        //获得南岸的合格率，并打印
+        Float val = qulifyRateMap.get("南岸");
+        if (val == null) {
+            logger.error("南岸val为空,单独合格率获取失败");
+            return -1;
+        }else {
+            logger.info(new StringBuilder().append("南岸 单独合格率 val:-------").append(val).toString());
+            for(Map.Entry<String, Float> entry : qulifyRateMap.entrySet()){
+                String mapKey = entry.getKey();
+                Float mapValue = entry.getValue();
+                System.out.println(mapKey+":"+mapValue);
+                logger.info(new StringBuilder().append(mapKey + " 单独合格率 val:-------").append(mapValue).toString());
+            }
+        }
+
+
+
+
+        //详细错误点数
+        String ersStr = this.getNananTestError();
+        List<ErrorResult> errorResultList = SvgUtil.getErrorResult(ersStr);
+
+
+
+        for (ErrorResult er1 : errorResultList) {
+            logger.info(
+                    new StringBuilder().append("错误点数详情：设备名称：")
+                    .append(er1.getSbName())
+                    .append(",估计值：")
+                    .append(er1.getGjValue())
+                    .append(",量测值：")
+                    .append(er1.getLcValue()).toString()
+            );
+        }
+        return val;
+    }
+}
--- src/main/java/com/services/dpidcalarm/collect/service/QuartzTask.java
+++ src/main/java/com/services/dpidcalarm/collect/service/QuartzTask.java
@@ -2,6 +2,7 @@ package com.services.dpidcalarm.collect.service;
 
 import com.services.dpidcalarm.collect.dao.IndicatorDataDao;
 import com.services.dpidcalarm.collect.job.CollectDataBDZYCSX;
+import com.services.dpidcalarm.collect.job.CollectDataDDHGL;
 import com.services.dpidcalarm.collect.job.CollectScoreByForm;
 import com.services.dpidcalarm.sms.SmsService;
 import com.services.dpidcalarm.utils.MyDateUtils;
@@ -52,12 +53,39 @@ public class QuartzTask {
 
 
 
-        logger.info("参数-是否发送：" + sendFlag);
+        logger.info("参数-是否发送短信：" + sendFlag);
         logger.info("参数-限值：" + limitValueStr);
         logger.info("参数-发送号码：" + phone);
         double limitValue =  Double.parseDouble(limitValueStr);
         long currentTime = System.currentTimeMillis();
 
+
+        /****单独合格率****/
+
+        try {
+            //指标数据获取
+            logger.info("单独合格率指标数据获取开始：");
+            CollectDataDDHGL collectData= new CollectDataDDHGL();
+            collectData.login(null,null,null);
+            String svgImage = collectData.getSvgImageForKHZB(null);
+            logger.info("单独合格率指标指标数据：svgImage：" + svgImage);
+            double scoreDDHGL =  collectData.getResultDDHGL(svgImage);
+            logger.info("单独合格率指标得分：" + scoreDDHGL);
+            if(scoreDDHGL < limitValue && 1==sendFlag){
+                smsService.sendSms(phone, "单独合格率指标得分" + scoreDDHGL + "，小于限值" + limitValueStr);
+                logger.info("单独合格率指标得分" + scoreDDHGL + "，小于限值" + limitValueStr + "发送短息到" + phone);
+            }
+            //存储指标
+            indicatorDataDao.insertIndicatorHisData(6,(float)scoreDDHGL , new Date(currentTime));
+            //更新实时
+            indicatorDataDao.insertIndicatorHisData(6,(float)scoreDDHGL , new Date(currentTime));
+
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            e.getMessage();
+        }
+
+        /**变电站遥测刷新指标**/
 
         try {
             //指标数据获取
@@ -125,7 +153,7 @@ public class QuartzTask {
             //存储历史指标
             indicatorDataDao.insertIndicatorHisData(2,(float)scoreKGZB , new Date(currentTime));
             //存储实时表
-            indicatorDataDao.updateIndicatorRtData(1,(float)scoreKGZB , new Date(currentTime));
+            indicatorDataDao.updateIndicatorRtData(2,(float)scoreKGZB , new Date(currentTime));
 
         }catch (Exception e){
             logger.error(e.getMessage());
@@ -162,9 +190,10 @@ public class QuartzTask {
 
 
         try {
-            //*************3、积分电量得分*************
+            //*************3、积分电量得分  昨日的值*************
             String scoreURL_jfdl = "http://10.55.6.114/analysis/TmrJFMonthCord_j.gc";
-            String compony_jfdl = "南岸分公司";
+            //String compony_jfdl = "南岸分公司";
+            String compony_jfdl = "%e5%8d%97%e5%b2%b8%e5%88%86%e5%85%ac%e5%8f%b8";
             //开始日期为当前时期
             String yesterdayStr = MyDateUtils.getYesterdayDayStr();
             String resultJSON_jfdl = collectScoreByForm.getCurrentScoreJSON(scoreURL_jfdl,compony_jfdl,null,yesterdayStr);
@@ -190,56 +219,6 @@ public class QuartzTask {
 
     }
 
-
-    // public void reptilian(){
-    //     //指标数据获取
-    //     System.out.println("指标数据获取："+new Date());
-    //     CollectScoreByForm collectScoreByForm = new CollectScoreByForm();
-    //     //**************1、登录*****************//
-    //     // 内部有写死的url，所有的指标登录都是一样的，本部分只做测试，后续从数据库读
-    //     collectScoreByForm.login(null,null,null);
-    //
-    //     /**
-    //      * 得分： 开关和遥测遥信只是表名不一样。 积分电量url和开始日期（昨天）和com（中文字符，不一样）
-    //      * @param scoreURL   开关和遥测遥信的url一致都是http://10.55.6.114/analysis/Daycord_j.gc， 积分电量url：http://10.55.6.114/analysis/TmrJFMonthCord_j.gc
-    //      * @param compony  开关和遥测遥信的是编号“113715891329302536”，积分电量的是“市区分公司”
-    //      * @param table  开关“bk_con_cord_5”  遥测遥信“excDeviceCord” 积分电量没有table
-    //      * @param startDay 开关和遥测是今天，积分电量是昨天
-    //      * @return
-    //      * @throws IOException
-    //      */
-    //
-    //     //url
-    //     String scoreURL = "http://10.55.6.114/analysis/Daycord_j.gc";
-    //     //公司
-    //     String compony = "113715891329302534";
-    //     //*************2、开关指标得分*************//
-    //     //开关表名
-    //     String table_kg = "bk_con_cord_5";
-    //     //开始日期为当前时期
-    //     String currentDay = MyDateUtils.getCurrentDayStr();
-    //     String resultJSON = collectScoreByForm.getCurrentScore(scoreURL,compony,table_kg,currentDay);
-    //     System.out.println("获取结果：resultJSON："+resultJSON);
-    //     collectScoreByForm.dealcurrentScore(resultJSON);
-    //
-    //     /*
-    //
-    //     //*************3、遥测遥信得分*************
-    //     //遥测遥信表名
-    //     String table_ycyx = "bk_con_cord_5";
-    //     //开始日期为当前时期
-    //     String resultJSON_ycyx = collectScoreByForm.getCurrentScore(scoreURL,compony,table_ycyx,currentDay);
-    //     System.out.println("获取结果：resultJSON："+resultJSON_ycyx);
-    //     collectScoreByForm.dealcurrentScore(resultJSON_ycyx);
-    //
-    //     //*************3、积分电量得分*************
-    //     //开始日期为当前时期
-    //     String resultJSON_jfdl = collectScoreByForm.getCurrentScore(scoreURL,compony,null,currentDay);
-    //     System.out.println("获取结果：resultJSON："+resultJSON_ycyx);
-    //     collectScoreByForm.dealcurrentScore(resultJSON_ycyx);
-    //     */
-    //
-    // }
 
 
 }
